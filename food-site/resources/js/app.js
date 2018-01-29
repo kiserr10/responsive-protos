07d8a8768a7c4f1d541bfdc02dc1e5042414f228
Ross Kiser <kiserr10@gmail.com>
2018-01-28 18:56:09 -0700

    sticky nav working on scroll

--- food-site/resources/js/app.js
+++ food-site/resources/js/app.js
@@ -1,3 +1,13 @@
 $(document).ready(() => {
-	console.log('jquery working');
+	//Sticky Nav
+	$('.js--section-features').waypoint((direction) => {
+		if(direction === 'down'){
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset: '60px'
+	});
+
 });
